3f1c45f87c7251c971cd0c84090d22291c0b38c5
Iago Rodriguez Lopez <iago.rodriguez.lopez@gmail.com>
2015-08-11 12:02:09 -0300

    Update validator.rb

--- lib/validator.rb
+++ lib/validator.rb
@@ -7,9 +7,9 @@ class RutValidator < ActiveModel::EachValidator
   # @param value [String] the string to be validated
   def validate_each(record, attribute, value)
     if value.nil? or not value.is_a?(String)
-      record.errors[attribute] << (options[:message] || "is not a valid rut")
+      record.errors[attribute] << (options[:message] || "no es un RUT válido")
     elsif not value.rut_valid?
-      record.errors[attribute] << (options[:message] || "is not a valid rut")
+      record.errors[attribute] << (options[:message] || "no es un RUT válido")
     end
   end
 end
